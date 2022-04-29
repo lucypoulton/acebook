86e15a11ae820230fde94450bcb0bddaccc19d3b
Lucy Poulton <lucy@lucypoulton.net>
2022-04-29 11:10:56 +0100

    cats. cats everywhere

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,9 @@ const HomeController = {
   Index: (req, res) => {
     res.redirect("posts");
   },
+  SuperSecret(req, res) {
+    res.render('super_secret');
+  }
 };
 
 module.exports = HomeController;
--- routes/home.js
+++ routes/home.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const HomeController = require("../controllers/home");
 
 router.get("/", HomeController.Index);
+router.get("/super-secret", HomeController.SuperSecret);
 
 module.exports = router;
